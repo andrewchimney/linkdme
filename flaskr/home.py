f0e5cad0ce7503500cb0f225cee1b36c75f0f30e
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2024-06-23 13:09:56 -0700

    home page

--- flaskr/home.py
+++ flaskr/home.py
@@ -1,7 +1,9 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+import os
 bp = Blueprint('/', __name__)
+ip = os.getenv("IP")
 @bp.route('/', methods =('GET','POST'))
 def index():
-     return render_template('home.html')
+     return render_template('home.html', ip=ip)
